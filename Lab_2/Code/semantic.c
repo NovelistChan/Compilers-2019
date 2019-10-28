94df14418cd849620360858d74b7db23e064616a
uziiiiiiii <2509212623@qq.com>
2019-10-28 05:19:55 -0700

    add some function in semantic.c

--- Lab_2/Code/semantic.c
+++ Lab_2/Code/semantic.c
@@ -3,3 +3,29 @@
 void semanticAnalysis(){
     Program(root);
 }
+
+void Program(TreeNode *node){
+    ExtDefList(node);
+}
+
+void ExtDefList(TreeNode *node){
+    TreeNode *child = node->children;
+    if(child){
+        ExtDef(child);
+        ExtDefList(child->next);
+    }
+}
+
+void ExtDef(TreeNode *node){
+    TreeNode *child = node->children;
+    Type type = Specifier(child);
+
+    child = child->next;
+    if(!strcmp(child->name, "ExtDecList")){
+        // TODO
+    }else if(!strcmp(child->name, "SEMI")){
+        // TODO
+    }else{
+        // TODO
+    }
+}
