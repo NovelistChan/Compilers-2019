f2c366d93d2062fea0ac61121e372c6426e03d06
uziiiiiiii <2509212623@qq.com>
2019-10-29 05:27:52 -0700

    normal update

---
+++ Lab_2/Code/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+extern void yyrestart();
+extern int yyparse();
+extern void createHashTable();
+extern void semanticAnalysis();
+extern bool is_pass;
+
+int main(int argc, char** argv){
+  if(argc<=1) return 1;
+
+  FILE* f = fopen(argv[1], "r");
+  if(!f){
+    perror(argv[1]);
+    return 1;
+  }
+
+  yyrestart(f);
+  yyparse();
+
+  if(is_pass){
+    // TODO initHashTable, semantic analysis
+    createHashTable();
+    semanticAnalysis();
+  }
+  return 0;
+}
+
